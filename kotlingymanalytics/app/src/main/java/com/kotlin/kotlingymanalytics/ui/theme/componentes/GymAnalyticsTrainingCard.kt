c67c0f8035ed33c370b093dba63db134700c81c7
alberto-lizana <alb.lizana@duocuc.cl>
2026-08-24 20:41:47 -0400

    pequeños arreglos

--- kotlingymanalytics/app/src/main/java/com/kotlin/kotlingymanalytics/ui/theme/componentes/GymAnalyticsTrainingCard.kt
+++ kotlingymanalytics/app/src/main/java/com/kotlin/kotlingymanalytics/ui/theme/componentes/GymAnalyticsTrainingCard.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kotlin.kotlingymanalytics.ui.theme.AzulOscuro
+import com.kotlin.kotlingymanalytics.ui.theme.FondoClaro
 
 @Composable
 fun GymAnalyticsTrainingCard(
@@ -24,7 +26,11 @@ fun GymAnalyticsTrainingCard(
     onStart: () -> Unit
 ) {
     Card(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = FondoClaro
+        )
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
